c3ee3610c6b8cb899b2ba44973668dbb81a7cd1b
Mohamad Elnomrossie <m.elnomrossie@gmail.com>
2021-05-25 23:09:28 +0200

    d

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap"
-
 import {Provider} from "react-redux"
 import {createStore} from 'redux'
 import reducer from "./reducers"
